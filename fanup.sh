0c96d81a8f690214318ecb1449022ead9e74a133
Adam Kaminski <adam@adamkaminski.com>
2013-08-19 02:18:17 -0400

    initial commit

---
+++ fanup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#	Slow fan in cr-48 prototype after resuming from sleep
+# 	@see https://bugzilla.redhat.com/show_bug.cgi?id=895276#c18
+#
+#	This may have be resolved in 3.9.2+ kernels
+#	
+#	-- Review temperature with 'sensors' command --
+#
+#	related scripts /etc/pm/sleep.d/20_fan_spindown_after_resume
+#
+
+#cd /sys/class/thermal/
+#echo 0 > cooling_device0/cur_state
+#echo 0 > cooling_device1/cur_state
+#echo 0 > cooling_device2/cur_state
+#echo 0 > cooling_device3/cur_state
+#echo 0 > cooling_device4/cur_state
+#echo 0 > cooling_device5/cur_state
+#echo 0 > cooling_device6/cur_state
+
+	echo "1" >/sys/class/thermal/cooling_device0/cur_state
+	echo "1" >/sys/class/thermal/cooling_device1/cur_state
+	echo "1" >/sys/class/thermal/cooling_device2/cur_state
+exit 0
+
